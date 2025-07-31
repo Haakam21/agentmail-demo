082356f28de293ec2ad3767483e02a13629f026b
Haakam Aujla <haakamsingh21@gmail.com>
2025-07-30 19:32:34 -0700

    Revert "Revert "Basic""
    This reverts commit f9d7e0f62d0e46e380f25c685a5634336c4c0e3d.

--- app.py
+++ app.py
@@ -1,22 +1,5 @@
 from agents import Agent, Runner
 from flask import Flask, request
-from agentmail import AgentMail, Message
-
-client = AgentMail()
-
-inbox = client.inboxes.create(
-    username="demo",
-    domain="agentmail.to",
-    client_id="demo-inbox",
-)
-
-webhook = client.webhooks.create(
-    url="https://agentmail.onrender.com/chat",
-    inbox_ids=[inbox.inbox_id],
-    event_types=["message.received"],
-    client_id="demo-webhook",
-)
-
 
 agent = Agent(
     name="Email Agent",
@@ -28,14 +11,6 @@ app = Flask(__name__)
 
 @app.post("/chat")
 def chat():
-    message = Message(**request.json["message"])
-
-    result = Runner.run_sync(agent, message.model_dump_json())
-
-    client.inboxes.messages.reply(
-        inbox_id=message.inbox_id,
-        message_id=message.message_id,
-        text=result.final_output,
-    )
+    result = Runner.run_sync(agent, request.json["message"])
 
     return result.final_output
