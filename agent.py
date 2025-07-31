da24fa64228fb9108d92da553be386c9a1423ce9
Haakam Aujla <haakamsingh21@gmail.com>
2025-07-30 18:53:06 -0700

    Initial commit

---
+++ agent.py
@@ -0,0 +1,35 @@
+from agents import Agent, Runner
+from flask import Flask, request
+from agentmail import AgentMail, Message
+
+client = AgentMail()
+
+inbox = client.inboxes.create(username="demo", domain="agentmail.to")
+
+webhook = client.webhooks.create(
+    inbox_id=inbox.inbox_id,
+    url="https://agentmail.onrender.com/chat",
+)
+
+
+agent = Agent(
+    name="Email Agent",
+    instructions="You are an email agent named AgentMail. Respond as if you are writing an email. Do not include the subject, only the body.",
+)
+
+app = Flask(__name__)
+
+
+@app.post("/chat")
+def chat():
+    message = Message(**request.json["message"])
+
+    result = Runner.run_sync(agent, message.model_dump_json())
+
+    client.inboxes.messages.reply(
+        inbox_id=message.inbox_id,
+        message_id=message.message_id,
+        text=result.final_output,
+    )
+
+    return result.final_output
